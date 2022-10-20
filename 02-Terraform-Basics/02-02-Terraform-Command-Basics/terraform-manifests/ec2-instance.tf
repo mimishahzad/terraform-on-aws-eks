9026bf8e17c5b4850d3f4a788a76019acd2ca034
Mimi Shahzad <shahz22t@mtholyoke.edu>
2022-10-20 04:13:21 -0400

    Adjusted VSCode settings and edited ec2-instance.tf inside of Terraform-Basics

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 # Resource Block
-resource "aws_instance" "ec2demo" {
-  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
+resource "aws_instance" "EC2-Floofy-Tutorial" {
+  ami           = "ami-0022f774911c1d690" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
